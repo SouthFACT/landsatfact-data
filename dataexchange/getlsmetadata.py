686a04fcaca29747d8c5c928e80c631bf0513298
Derek Morgan <geoderek@git>
2014-12-30 23:48:34 +0000

    move connection outside of for loop

--- dataexchange/getlsmetadata.py
+++ dataexchange/getlsmetadata.py
@@ -9,7 +9,7 @@ try:
     from Config import *
 except:
     print "Cannot find local settings file 'Config.py'.  You need to create a Config.py file that contains"
-    print "settings appropriate for this copy of the FSWMS project.  You can use the file 'Config.tpl.py'"
+    print "settings appropriate for this copy of the project.  You can use the file 'Config.tpl.py'"
     print "as a starting point --- make a copy of that file called 'Config.py', and edit appropriately."
     exit(-1)
 
@@ -17,13 +17,17 @@ except:
 # metadataFile = urllib.URLopener()
 # metadataFile.retrieve("http://landsat.usgs.gov/metadata_service/bulk_metadata_files/LANDSAT_8.csv", "LANDSAT_8.csv")
 
-# Loop through CSV for LANDSAT_8 inserting into PGSQL
-filename = 'LANDSAT_8.csv'
-# filename = 'test.csv'
+# Loop through CSV inserting into PGSQL
+# LANDSAT_8 
+# filename = 'LANDSAT_8.csv'
+# LANDSAT_7 
+filename = 'LANDSAT_ETM_SLC_OFF.csv'
+
 with open(filename, 'rb') as f:
     reader = csv.reader(f)
     try:
         rowCount = 0
+        conn = psycopg2.connect(POSTGIS_CONNECTION_STRING_DATA)
         for row in reader:
             if rowCount>0:
                 sceneID = row[0]
@@ -38,7 +42,6 @@ with open(filename, 'rb') as f:
                 ccFullLL = row[22] or None
                 ccFullLR = row[23] or None
                 dataTypeL1 = row[53]
-                conn = psycopg2.connect(POSTGIS_CONNECTION_STRING_DATA)
                 cur = conn.cursor()
                 # check to see if the record already exist
                 sql = 'SELECT count(*) from landsatmetadata WHERE sceneid = %s;'
@@ -49,13 +52,15 @@ with open(filename, 'rb') as f:
                 print result[0]
                 if result[0]==0:
                     # do the insert
+                    print "Inserting..."
+                    print sceneID+sensor+acquisitionDate+browseURL+path+rowNum+ccFull+ccFullUL+ccFullUR+ccFullLL+ccFullLR+dataTypeL1
                     cur = conn.cursor()
                     cur.execute("INSERT INTO landsatmetadata VALUES \
                         (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", \
                         (sceneID,sensor,acquisitionDate,browseURL,path,rowNum,ccFull,ccFullUL,ccFullUR,ccFullLL,ccFullLR,dataTypeL1)) # correct
                     conn.commit()
                     cur.close()
-                conn.close()
             rowCount = rowCount+1
+        conn.close()
     except csv.Error as e:
         sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
