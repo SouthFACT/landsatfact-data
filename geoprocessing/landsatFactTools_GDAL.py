7b0dfb5834eb75b71f324ab323511e1a3915b30d
Derek Morgan <geoderek@git>
2015-06-18 16:12:45 +0000

    text about public domain issue #39

--- geoprocessing/landsatFactTools_GDAL.py
+++ geoprocessing/landsatFactTools_GDAL.py
@@ -7,7 +7,15 @@
 # 
 # Created:     03/25/2015
 # Copyright:   (c) mclebr 2014
-# Licence:     <your licence>
+# Licence:     
+""" This code was developed in the public domain.
+This code is provided "as is", without warranty of any kind,
+express or implied, including but not limited to the warranties of
+merchantability, fitness for a particular purpose and noninfringement.
+In no event shall the authors be liable for any claim, damages or
+other liability, whether in an action of contract, tort or otherwise,
+arising from, out of or in connection with the software or the use or
+other dealings in the software."""
 #-------------------------------------------------------------------------------
 
 import os, sys, tarfile, shutil, traceback, gzip, subprocess, stat, datetime
