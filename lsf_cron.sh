8c7c5cb2e07a564b2d81a8a4d22f31d011c5f184
Matt Hutchins <mwhutchi@unca.edu>
2015-10-21 19:03:32 +0000

    changed reference from landsatFACTmain to landsatFACT_LCV

--- lsf_cron.sh
+++ lsf_cron.sh
@@ -7,7 +7,7 @@ php update_landsat_metadata.php
 php download_landsat_data.php
 
 cd /var/vsites/landsatfact-data-dev.nemac.org/project/geoprocessing
-./landsatFACTmain.py
+./landsatFACT_LCV.py
 
 cd /var/vsites/landsatfact-data-dev.nemac.org/project/msconfig
 ./make_latest_mosaics.sh
