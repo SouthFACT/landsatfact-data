c2ec98e7f23f2746cea8ea7f34ac1560654ad441
Daviesm <daveism@gmail.com>
2015-11-24 16:19:52 +0000

    Fix for dataset names

--- dataexchange/download_landsat_data.php
+++ dataexchange/download_landsat_data.php
@@ -5,7 +5,7 @@
    License: This code was developed in the public domain. This code is provided "as is", without warranty of any kind,
 	express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement.
 	In no event shall the authors be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise,
-	arising from, out of or in connection with the software or the use or other dealings in the software. 
+	arising from, out of or in connection with the software or the use or other dealings in the software.  
 */
 include 'usgseros.php';
 
