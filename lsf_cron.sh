93541cb34cbb13c9dd667b78670d9e208905c35b
Matt Hutchins <mwhutchi@unca.edu>
2015-12-04 17:46:30 +0000

    changed to not currently run mosaic creation

--- lsf_cron.sh
+++ lsf_cron.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 
-#cd /var/vsites/landsatfact-data-dev.nemac.org/project/dataexchange
-#php update_landsat_metadata.php
+cd /var/vsites/landsatfact-data-dev.nemac.org/project/dataexchange
+php update_landsat_metadata.php
 
-#php download_landsat_data.php
+php download_landsat_data.php
 
-#cd /var/vsites/landsatfact-data-dev/project
-#./checkTarForDir.py
+cd /var/vsites/landsatfact-data-dev.nemac.org/project
+./checkTarForDir.py
 
-#cd /var/vsites/landsatfact-data-dev.nemac.org/project/geoprocessing
-#./landsatFACT_LCV.py
+cd /var/vsites/landsatfact-data-dev.nemac.org/project/geoprocessing
+./landsatFACT_LCV.py
 
 #cd /var/vsites/landsatfact-data-dev.nemac.org/project/msconfig
 #./make_latest_mosaics.sh
