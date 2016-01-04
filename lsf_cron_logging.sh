6c1d0244b5ae12719ca02f6beac3fa9bd17269a6
Matt Hutchins <mwhutchi@unca.edu>
2016-01-04 19:19:21 +0000

    added wrapper around existing customRequest.py call that will 1) prevent CR from running while another CR is running and 2) prevent CR from running while LCV is running

--- lsf_cron_logging.sh
+++ lsf_cron_logging.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
-cd /var/vsites/landsatfact-data-dev.nemac.org/project
-./lsf_cron.sh > /var/vsites/landsatfact-data-dev.nemac.org/project/var/log/lsf_cron.log 2>&1
+
+me=`basename "$0"`
+if [ `ps ax | grep $me | wc -l` -le 3 ]; then
+        COUNTER=0
+        until [[ `ps ax | grep initate_custom_ | wc -l` -lt 2 || $COUNTER -eq 10 ]]; do
+                sleep 120
+                let COUNTER=COUNTER+1
+        done
+        if [ $COUNTER -lt 10 ]; then
+		cd /var/vsites/landsatfact-data-dev.nemac.org/project
+		./lsf_cron.sh > /var/vsites/landsatfact-data-dev.nemac.org/project/var/log/lsf_cron.log 2>&1
+	fi
+fi
