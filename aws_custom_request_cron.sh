0b0e3cb3ba5c1946a4869187139be2556762cf8a
Daviesm <daveism@gmail.com>
2016-01-23 00:12:55 +0000

    remove CR local zips older than 45 days

--- aws_custom_request_cron.sh
+++ aws_custom_request_cron.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 
-export AWS_CONFIG_FILE /var/vsites/landsatfact-data-dev.nemac.org/project/aws.config
+export AWS_CONFIG_FILE=/var/vsites/landsatfact-data-dev.nemac.org/project/aws.config
+/usr/bin/aws s3 sync /lsfdata/products/cr_zips  s3://landsat-cr-products --include "*.zip" 
 
-/usr/bin/aws s3 cp /lsfdata/products/cr_zips  s3://landsat-cr-products --recursive --include "*.zip" 
+#remove custom requests over a 45 days old.
+find /lsfdata/products/cr_zips/*.zip -mtime +45  -exec rm {} \;
 
