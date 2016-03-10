6ff7b9e6129327caf3606aa0a3c388e1cc4110ab
Matt Hutchins <mwhutchi@unca.edu>
2016-03-10 16:28:26 +0000

    added thumbnails_cron

---
+++ aws_thumbnails_cron.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+
+export AWS_CONFIG_FILE=/var/vsites/landsatfact-data-dev.nemac.org/project/aws.config
+/usr/bin/python /var/vsites/landsatfact-data-dev.nemac.org/project/dataexchange/make_thumbnails.py
+
