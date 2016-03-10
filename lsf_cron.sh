0e67e0e4ebacc46cd16aaead7dddb20026538193
Matt Hutchins <mwhutchi@unca.edu>
2016-03-10 16:42:10 +0000

    changes to scripts running in production

--- lsf_cron.sh
+++ lsf_cron.sh
@@ -1,18 +1,13 @@
 #! /bin/bash
 
-
-cd /var/vsites/landsatfact-data-dev.nemac.org/project/dataexchange
+#going to landsatfact-data production repository and running scripts there
+cd /var/vsites/landsatfact-data.nemac.org/project/dataexchange
 php update_landsat_metadata.php
 
 php download_landsat_data.php
 
-cd /var/vsites/landsatfact-data-dev.nemac.org/project
-./checkTarForDir.py
-
-cd /var/vsites/landsatfact-data-dev.nemac.org/project/geoprocessing
+cd /var/vsites/landsatfact-data.nemac.org/project/geoprocessing
 ./landsatFACT_LCV.py
 
-#cd /var/vsites/landsatfact-data-dev.nemac.org/project/msconfig
-#./make_latest_mosaics.sh
-
-#./makeviewerconfig.py
+cd /var/vsites/landsatfact-data.nemac.org/project/msconfig
+./makeviewerconfig.py
