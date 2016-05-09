60cde1741253dc672f323592bbe10f2f3112cdc8
Daviesm <daveism@gmail.com>
2016-05-09 14:33:45 +0000

    this fixes #56 and fixes #57

--- lsf_cron_logging.sh
+++ lsf_cron_logging.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+#get the config file and make sure it will not do something delete all...
+configfile='./bash_config.cfg'
+configfile_secured='./tmp_bash_config.cfg'
+
+# check if the file contains something we don't want
+if egrep -q -v '^#|^[^ ]*=[^;]*' "$configfile"; then
+  echo "Config file is unclean, cleaning it..." >&2
+  # filter the original to a new file
+  egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
+  configfile="$configfile_secured"
+fi
+
+# now source it, either the original or the filtered variant
+source "$configfile"
+
 me=`basename "$0"`
 if [ `ps ax | grep $me | wc -l` -le 3 ]; then
         COUNTER=0
@@ -8,7 +23,7 @@ if [ `ps ax | grep $me | wc -l` -le 3 ]; then
                 let COUNTER=COUNTER+1
         done
         if [ $COUNTER -lt 10 ]; then
-		cd /var/vsites/landsatfact-data.nemac.org/project
-		./lsf_cron.sh > /var/vsites/landsatfact-data.nemac.org/project/var/log/lsf_cron.log 2>&1
+		cd $path_projects
+		./lsf_cron.sh > $path_log/lsf_cron.log 2>&1
 	fi
 fi
