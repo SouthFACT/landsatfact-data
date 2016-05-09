b448ca34684a4292fab3ec3f0a9ca12fa473272c
Daviesm <daveism@gmail.com>
2016-05-09 17:47:53 +0000

    this closes #63

--- geoprocessing/fmaskLinux/runFmask.sh
+++ geoprocessing/fmaskLinux/runFmask.sh
@@ -1,6 +1,23 @@
+#! /bin/bash
+
+#get the config file and make sure it will not do something delete all...
+configfile='./bash_config.cfg'
+configfile_secured='./tmp_bash_config.cfg'
+
+# check if the file contains something we don't want
+if egrep -q -v '^#|^[^ ]*=[^;]*' "$configfile"; then
+  echo "Config file is unclean, cleaning it..." >&2
+  # filter the original to a new file
+  egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
+  configfile="$configfile_secured"
+fi
+
+#  now source it, either the original or the filtered variant
+source "$configfile"
+
 export LD_LIBRARY_PATH=/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/bin/glnxa64:/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/sys/java/jre/glnxa64/jre/lib/amd64
 export XAPPLRESDIR=/usr/local/MATLAB/MATLAB_Compiler_Runtime/v80/X11/app-defaults
 echo $LD_LIBRARY_PATH
 echo $XAPPLRESDIR
-/var/vsites/landsatfact-data.nemac.org/project/geoprocessing/fmaskLinux/Fmask_Linux
+$path_projects/geoprocessing/fmaskLinux/Fmask_Linux
 # ./Fmask_Linux
