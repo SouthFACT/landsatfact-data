fc32baf1f7a446d18d075880a86c572a22cf776f
Daviesm <daveism@gmail.com>
2016-05-10 15:25:12 +0000

    this closes #82

--- msconfig/mosaic_overviews.sh
+++ msconfig/mosaic_overviews.sh
@@ -5,7 +5,28 @@
 ###  it simply creates gdal overviews on the latest change mosaics.
 ###
 
-cd /lsfdata/products/mosaics/temp
+
+DIRECTORY=`dirname $0`
+echo $DIRECTORY
+
+cd $DIRECTORY
+cd ..
+
+#get the config file and make sure it will not do something delete all...
+configfile=./bash_config.cfg
+configfile_secured=./tmp_bash_config.cfg
+
+# check if the file contains something we don't want
+if egrep -q -v '^#|^[^ ]*=[^;]*' "$configfile"; then
+  # filter the original to a new file
+  egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
+  configfile="$configfile_secured"
+fi
+
+#  now source it, either the original or the filtered variant
+source "$configfile"
+
+cd $path_products/mosaics/temp
 
 for f in *.tif
 do
