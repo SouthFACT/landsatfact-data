c4ffa776689d8b9ac8aff53e153d313d230f757f
Daviesm <daveism@gmail.com>
2016-05-10 13:36:50 +0000

    this closes #69, closes #70, closes #71

--- lsf_cron.sh
+++ lsf_cron.sh
@@ -1,13 +1,30 @@
 #! /bin/bash
 
+DIRECTORY=`dirname $0`
+echo $DIRECTORY
+
+#get the config file and make sure it will not do something delete all...
+configfile=$DIRECTORY/bash_config.cfg
+configfile_secured=$DIRECTORY/tmp_bash_config.cfg
+
+# check if the file contains something we don't want
+if egrep -q -v '^#|^[^ ]*=[^;]*' "$configfile"; then
+  # filter the original to a new file
+  egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
+  configfile="$configfile_secured"
+fi
+
+#  now source it, either the original or the filtered variant
+source "$configfile"
+
 #going to landsatfact-data production repository and running scripts there
-cd /var/vsites/landsatfact-data.nemac.org/project/dataexchange
+cd $path_projects/dataexchange
 php update_landsat_metadata.php
 
 php download_landsat_data.php
 
-cd /var/vsites/landsatfact-data.nemac.org/project/geoprocessing
+cd $path_projects/geoprocessing
 ./landsatFACT_LCV.py
 
-cd /var/vsites/landsatfact-data.nemac.org/project/msconfig
+cd $path_projects/msconfig
 ./makeviewerconfig.py
