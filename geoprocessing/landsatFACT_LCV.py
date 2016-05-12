f3e79e002d69ae60f39d0819d6067e8f919ad2b8
Daviesm <daveism@gmail.com>
2016-05-12 12:50:32 +0000

    trying fix dev git

--- geoprocessing/landsatFACT_LCV.py
+++ geoprocessing/landsatFACT_LCV.py
@@ -12,7 +12,7 @@
  In no event shall the authors be liable for any claim, damages or
  other liability, whether in an action of contract, tort or otherwise,
  arising from, out of or in connection with the software or the use or
- other dealings in the software.'''
+ other dealings in the software.''' 
 #-----------------------------------------------------------------------------
 def main():
     pass
