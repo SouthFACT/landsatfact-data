da6d5ae86eedaee8b0da78c6f838facefe83699e
Daviesm <daveism@gmail.com>
2016-05-13 20:19:05 +0000

    cron job intended for dev that imports the metadata each day

---
+++ lsf_metadata_cron.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+DIRECTORY=`dirname $0`
+echo $DIRECTORY
+
+#get the config file and make sure it will not do something delete all...
+configfile=$DIRECTORY/bash_config.cfg
+configfile_secured=$DIRECTORY/tmp_bash_config.cfg
+
+# check if the file contains something we don't want
+if egrep -q -v '^#|^[^ ]*=[^;]*' "$configfile"; then
+  # filter the original to a new file
+  egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
+  configfile="$configfile_secured"
+fi
+
+#  now source it, either the original or the filtered variant
+source "$configfile"
+
+#only import metadata intended for dev enviroment so LSF metaddata is up-to-date....
+cd $path_projects/dataexchange
+php update_landsat_metadata.php
+
