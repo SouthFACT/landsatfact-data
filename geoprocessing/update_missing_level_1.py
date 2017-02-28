354208650ea5f93141ce731629ddea36d1654db8
Daveism <daveism@gmail.com>
2017-02-28 18:30:23 +0000

    fix for missing level_1 metadata over past three days

--- geoprocessing/update_missing_level_1.py
+++ geoprocessing/update_missing_level_1.py
@@ -42,6 +42,7 @@ statement = "SELECT {0} FROM {1};".format(tableColumnList[0],tableName)
 resultsTup = landsatFactTools_GDAL.postgresCommand(statement)
 
 print resultsTup
+print '' 
 
 runList=[]
 extractedList = []
@@ -54,8 +55,8 @@ for scene in resultsTup:
 for scene in runList:
     try:
         # sets full path for the tarfile to be analyzed
-        inNewSceneTar = os.path.join(tarStorage, scene)
-        extractedPath = os.path.join(tiffsStorage, scene, scene+ '_MTL.xt')
+        inNewSceneTar = os.path.join(tarStorage, scene + '.tar.gz')
+        extractedPath = os.path.join(tiffsStorage, scene, scene+ '_MTL.txt')
 
         print 'Looking for mtl file in ' + extractedPath
 
@@ -63,16 +64,46 @@ for scene in runList:
         if os.path.exists(extractedPath) == True:
            print "extracting MTL level 1 metadata from: " + extractedPath
 
-           mtlFile = rasterAnalysis_GDAL.mtlData(sceneID, extractedPath)           
+           mtlFile = rasterAnalysis_GDAL.mtlData(scene, extractedPath)           
            print 'Info: level 1 metadata for scene ' + scene + ' was updated.'
         else:
-	   print 'Warning: The level 1 metadata file  (' + extractedPath + ') did not exist.  You may need re-download and extract scene '+ scene +'.'
-        
-        print ''
+           if os.path.exists(inNewSceneTar) == True:
+              print 'tar ' + inNewSceneTar + ' exists extracting'
+              extractedPath = landsatFactTools_GDAL.checkExisting(inNewSceneTar, tiffsStorage)
+              extractedPath = os.path.join(tiffsStorage, scene, scene+ '_MTL.txt')
+            
+              mtlFile = rasterAnalysis_GDAL.mtlData(scene, extractedPath)
+              print 'Info: level 1 metadata for scene ' + scene + ' was updated.'
 
+           else: 
+              print 'tar ' + inNewSceneTar + ' does not exist will attempt download '
+
+              landsatFactTools_GDAL.retry(1, 4, landsatFactTools_GDAL.DownloadError,landsatFactTools_GDAL.downloadScene, scene )
+              
+              print 'check if download completed for ' + inNewSceneTar 
+              if os.path.exists(inNewSceneTar) == True:
+
+                  print 'Download completed for ' + inNewSceneTar
+                  extractedPath = landsatFactTools_GDAL.checkExisting(inNewSceneTar, tiffsStorage)
+                  extractedFile = os.path.join(tiffsStorage, scene, scene+ '_MTL.txt')
+
+                  print "extracting MTL level 1 metadata from: " + extractedFile
+                  mtlFile = rasterAnalysis_GDAL.mtlData(scene, extractedFile)
+
+                  print 'removing tar ' + inNewSceneTar
+                  os.remove(inNewSceneTar)
+
+                  print 'cleaning directory ' + extractedPath
+                  landsatFactTools_GDAL.cleanDir(extractedPath) 
+              else:
+	          print 'Warning: The level 1 metadata file  (' + extractedPath + ') did not exist.  You may need re-download and extract scene '+ scene +'.'
+
+        print ''   
+  
     except BaseException as e:
         print "Error in updating level 1 metadata"
         print str(e)
+
 
 print 'Done updating level 1 metadata'
 
