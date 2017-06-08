04efe329656f1c29b68299112900624a3e957cf0
daveoloughlin <oloughlind@bellsouth.net>
2017-06-08 13:34:44 +0000

    working version of AOI alerts

---
+++ geoprocessing/LSF_AOI_alerts.py
@@ -0,0 +1,141 @@
+#! /usr/bin/python
+#-------------------------------------------------------------------------------
+# Name:         LSF_AOI_alerts.py
+# Purpose:      LandsatFACT driver of LSF_zonalstats script
+#
+# Author:       LandsatFACT Project Team
+#               support@landsatfact.com
+'''This code was developed in the public domain.  This code is provided as is, without warranty of any kind,
+ express or implied, including but not limited to the warranties of
+ merchantability, fitness for a particular purpose and noninfringement.
+ In no event shall the authors be liable for any claim, damages or
+ other liability, whether in an action of contract, tort or otherwise,
+ arising from, out of or in connection with the software or the use or
+ other dealings in the software.'''
+#-----------------------------------------------------------------------------
+import os
+import LSF, landsatFactTools_GDAL, subprocess
+from collections import defaultdict
+import csv
+import re
+import pdb
+
+def writeListToFile (aList, filename):
+    with open(filename, "w") as text_file:
+        for item in aList:
+            text_file.write("%s\n" % item)
+
+def input2Date(swirFilename):
+    m=re.search('L\S*SWIR\.tif', swirFilename)
+    if m:
+        return m.group()[33:40]
+    else:
+        return None
+
+def writeRemapCSV(csvFilename):
+    with open(csvFilename, 'wb') as csvfile:
+        fieldnames = ['from lower', 'from upper', 'to']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'from lower': 247, 'from upper': 255, 'to': 4})
+        writer.writerow({'from lower': 227, 'from upper': 247, 'to': 3})
+        writer.writerow({'from lower': 207, 'from upper': 227, 'to': 2})
+        writer.writerow({'from lower': 187, 'from upper': 207, 'to': 1})
+
+def callZonalStats(aoiID, swirs, cloudMasks, gapMasks, statsOutPath):
+    print aoiID, swirs, cloudMasks, gapMasks
+    in_dir = os.getcwd()
+    os.chdir('/tmp')
+    print os.getcwd()
+    sqlString = 'select st_asgeojson(geom) from aoi_alerts where aoi_id={};'.format(aoiID)
+    resultsTup = landsatFactTools_GDAL.postgresCommand(sqlString)
+    if resultsTup:
+        geo = resultsTup[0][0]
+        writeListToFile([geo],'tmpAOIFile')
+    
+    writeListToFile(map(lambda x: os.path.join(LSF.productStorage, 'swir', x),swirs),'tmpSwirsFile')
+    writeListToFile(map(lambda x: os.path.join(LSF.productStorage, 'cloud_mask', x),cloudMasks),'tmpCloudMasksFile')
+    writeListToFile(map(lambda x: os.path.join(LSF.productStorage, 'gap_mask', x),gapMasks),'tmpGapMasksFile')
+    writeRemapCSV('remap.csv')
+
+    codeIn = [LSF.path_projects + '/geoprocessing/daveo/LSF_zonalstats.py', 'tmpAOIFile', 
+              'tmpSwirsFile', 'remap.csv', statsOutPath, '-c', 'tmpCloudMasksFile', '-g', 'tmpGapMasksFile']
+    process = subprocess.Popen(codeIn,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out,err = process.communicate()
+    errcode = process.returncode
+    os.chdir(in_dir)
+    print out, err
+
+def statsForAOINewProducts():
+    """
+    # Function to retrieve areas of interest with new products, call a function to determine the ammount of
+    #  change, and record the statistics for those areas of change in the DB
+    # @param None
+    # @return None
+    """
+    aDict = defaultdict(list)
+
+    resultsTup = landsatFactTools_GDAL.postgresCommand('SELECT * FROM vw_aoi_alerts;')
+    if resultsTup:
+        for k, v in resultsTup:
+            aDict[k].append(v)
+        for k, v in aDict.iteritems():
+            aoi = k
+            swirs = list(filter(lambda x: 'SWIR' in x, aDict[k]))
+            eventDateString=sorted(map(input2Date, swirs))[-1]
+            sqlString='select (date \'{0}-01-01\' + integer \'{1}\' - 1);'.format(eventDateString[:4], eventDateString[-3:])
+            resultsTup = landsatFactTools_GDAL.postgresCommand(sqlString)
+            dateString = resultsTup[0][0]
+            cloudMasks = list(filter(lambda x: 'Fmask' in x, aDict[k]))
+            gapMasks = list(filter(lambda x: 'GapMask' in x, aDict[k]))
+            sqlString = "SELECT st_area(geom::geography) from aoi_alerts where aoi_id={0}".format(aoi)
+            resultsTup = landsatFactTools_GDAL.postgresCommand(sqlString)
+            aoiAcres=float(resultsTup[0][0])*0.0002471044
+            if aoiAcres > 50000:
+                print 'Skipped aoi_id {0} because it is too large, {1} acres'.format(aoi, aoiAcres)
+                continue
+            sqlString = "SELECT exists (SELECT true FROM aoi_events WHERE aoi_id = {0} AND event_date = \'{1}\');".format(aoi,dateString)
+            existingRow = landsatFactTools_GDAL.postgresCommand(sqlString)[0][0]
+            if existingRow:
+                print 'aoi_event for {0} on {1} has already been run'.format(aoi, dateString)
+                continue
+
+            # status = "Process Start"
+            sqlString='insert into aoi_events(aoi_event_id, aoi_id, alert_status_id) VALUES(DEFAULT,\'{}\', 2) RETURNING aoi_event_id;'.format(aoi)
+            resultsTup = landsatFactTools_GDAL.postgresCommand(sqlString)
+            event_id = resultsTup[0][0]
+            callZonalStats(aoi, swirs, cloudMasks, gapMasks, 'tmpStatsFile.csv')
+            stats=[]
+            with open('/tmp/tmpStatsFile.csv', 'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    s=(row['Sum'])
+                    if s:
+                        h=int(float(s))
+                    else:
+                        h=0
+                    stats.append((int(row['Unique region ID']), int(row['Area in square meters']), 
+                                 int(row['Raster value']), h, int(row['Count']), int(row['Count*maxVal'])))
+            #sorted_by_region_ID = sorted(stats, key=lambda tup: tup[0])
+            def patchVal(v):return filter(lambda tup: tup[2]==v, stats)
+            sumArea=reduce(lambda x,y: x+y, map(lambda x: x[1], stats), 0)*0.0002471044
+            maxArea=reduce(lambda x,y: max(x,y), map(lambda x: x[1], stats), 0)*0.0002471044
+            acresChange=reduce(lambda x,y: x+y, map(lambda x: x[1], patchVal(1)), 0)*0.0002471044
+            acresAnalyzed=acresChange + reduce(lambda x,y: x+y, map(lambda x: x[1], patchVal(0)), 0)*0.0002471044
+            percentAnalyzed=float(acresChange)/float(aoiAcres)*100
+            percentChange=(float(acresChange)/float(acresAnalyzed))*100
+            largestPatch=reduce(lambda x,y: max(x,y), map(lambda x: x[1], patchVal(1)), 0)*0.0002471044
+            patchCount=len(patchVal(1))
+            smallestPatch=reduce(lambda x,y: min(x,y), map(lambda x: x[1], patchVal(1)), 0)*0.0002471044
+            resultsTup = landsatFactTools_GDAL.postgresCommand('select * from write_aoi_events({0},{1},{2},{3},{4},{5},{6},{7},{8},\'{9}\''.format(
+                                                                aoi, acresChange, percentChange, acresAnalyzed, percentAnalyzed,
+                                                                smallestPatch, largestPatch, patchCount, event_id, dateString)+ ',\''+ ','.join(swirs) + '\');')
+            if resultsTup[0][0] == False:
+                print 'Table update failed'
+            else:
+                # status = "Process Complete"
+                sqlString='UPDATE aoi_events set ("alert_status_id") = (3) where aoi_event_id={0};'.format(event_id)
+                resultsTup = landsatFactTools_GDAL.postgresCommand(sqlString)
+ 
+
+statsForAOINewProducts()
--- geoprocessing/LSF_zonalstats.py
+++ geoprocessing/LSF_zonalstats.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 #-------------------------------------------------------------------------------
-# Name:         LSF_zonalstats.py.py
+# Name:         LSF_zonalstats.py
 # Purpose:      LandsatFACT application script that derives statitics on changes witin an Area of Interest
 #
 # Author:       LandsatFACT Project Team
@@ -19,17 +19,19 @@ import sys, os, fnmatch, glob
 from osgeo import gdal
 from osgeo import ogr
 from osgeo import osr
+from rasterstats import zonal_stats
 import numpy as np
 import subprocess
 import argparse, csv
+from LSF import *
 import pdb
 
 
 # Utility function to provide the command line arguments to the script
-#  SWIR product, gap, or cloud mask
+#  AOI, SWIR product, reclass table, and stats output files  along with optional gap and/or cloud mask
 # @param
 #     None
-# @return None
+# @return list of arguments
 # Example call: [aoi, swirTIFFsFile, reclassTable, cloudMasksFile, gapMasksFile, statsResultTable] = parse_cmd_line()
 def parse_cmd_line():
     """
@@ -82,6 +84,16 @@ def writeArrayAsTIFF(array, outpath, rasterXSize, rasterYSize, gdalType, transfo
     dataset.SetProjection(projection)
     outBand.FlushCache()
 
+def cropToQuad(tiffFileIn, tiffFileOut, wrs2QuadID):
+    quadFull = os.path.join(quadsFolder, 'wrs2_'+wrs2QuadID[3:9]+wrs2QuadID[21:23]+'.shp')
+    #gdalwarp LC80250402017112LGN00UL_LE70250402017120EDC00ULtemp.tif LC80250402017112LGN00UL_LE70250402017120EDC00ULtemp2.tif -cutline wrs2_025040UL.shp -crop_to_cutline -tr  30 30 -tap -overwrite -co COMPRESS=LZW -dstnodata 255
+    codeIn = ['gdalwarp', '-cutline', quadFull, '-crop_to_cutline','-tr','30','30','-tap','-overwrite', '-co', 'COMPRESS=LZW', '-dstnodata', '255', tiffFileIn, tiffFileOut]
+    process = subprocess.Popen(codeIn,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out,err = process.communicate()
+    errcode = process.returncode
+    os.remove(tiffFileIn)
+
+
 # Utility function to add a key/value pair to a dictionary where the key is part of the value
 # @param
 #     [dictionary] - an existing dictionary
@@ -133,23 +145,17 @@ def applyMasks(srcArray, cloudMaskFile, gapMaskFile):
     """
     return maskArray(maskArray(srcArray, cloudMaskFile), gapMaskFile)
 
-# Utility function to threshold the product raster based on a CSV file
+# Utility function parse a reclass table in CSV format into its columns 1) from lower, 2)from upper, and 3) to.
 # @param
-#     [srcArray] - a numpy array
-#     [reclassTable] - full path to the raster mask or None
-# @return None
-# Example call: reclass(srcBandArray, 'C:\Users\olouda\remap.csv')
-def reclass(srcArray, reclassTable):
-    """
-    reclassTable is a CSV file which can contain the simplest reclassification table syntax (e.g., Explicit input ranges
-     from (http://desktop.arcgis.com/en/arcmap/10.3/tools/spatial-analyst-toolbox/using-reclassification-tables.htm).
-     The code assusmes the CSV file has headers.
-    """
+#     [reclassCSV] - full path to the reclassification table
+# @return lists for each column in the table
+# Example call: [lowerLimitList, upperLimitList, toValueList] = reclassCSVColumns('remap.csv')	
+def reclassCSVColumns(reclassCSV):
     lowerLimitList = []
     upperLimitList = []
     toValueList = []
 
-    with open(reclassTable, 'rb') as csvfile:
+    with open(reclassCSV, 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             #from lower, from upper, to
@@ -159,6 +165,21 @@ def reclass(srcArray, reclassTable):
             lowerLimitList.append(fromLower)
             upperLimitList.append(fromUpper)
             toValueList.append(toValue)
+	return [lowerLimitList, upperLimitList, toValueList]
+	
+# Utility function to threshold the product raster based on a CSV file
+# @param
+#     [srcArray] - a numpy array
+#     [reclassTable] - full path to the reclassification table
+# @return the reclassed numby array
+# Example call: reclass(srcBandArray, 'C:\Users\olouda\remap.csv')
+def reclass(srcArray, reclassTable):
+    """
+    reclassTable is a CSV file which can contain the simplest reclassification table syntax (e.g., Explicit input ranges
+     from (http://desktop.arcgis.com/en/arcmap/10.3/tools/spatial-analyst-toolbox/using-reclassification-tables.htm).
+     The code assusmes the CSV file has headers.
+    """
+    [lowerLimitList, upperLimitList, toValueList] = reclassCSVColumns(reclassTable)
     # reclassification
     sortedLower= sorted(lowerLimitList)
     sortedUpper = sorted(upperLimitList, reverse=True)
@@ -168,7 +189,6 @@ def reclass(srcArray, reclassTable):
     srcArray[(srcArray <= lowest)] = 0
     #if (highest <= srcArray.max()):
     srcArray[(srcArray >= highest)] = 0
-    srcArray[(srcArray > fromLower) & (srcArray <= fromUpper)] = toValue
     for fromLower, fromUpper, toValue in zip (lowerLimitList, upperLimitList, toValueList):
         srcArray[(srcArray > fromLower) & (srcArray <= fromUpper)] = toValue
     return srcArray
@@ -182,7 +202,8 @@ def reclass(srcArray, reclassTable):
 def mosaic(inRasterList, outRasterPath):
 
     # gdal_merge.py -o out.tif bar.tif temp.tif
-    codeIn = ['gdal_merge.py','-o', outRasterPath] + reclassedTIFFs
+    #codeIn = ['gdal_merge.py','-o', outRasterPath] + inRasterList
+    codeIn = ['gdalwarp','-dstnodata', '255'] + inRasterList + [outRasterPath]
     process = subprocess.Popen(codeIn,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out,err = process.communicate()
     errcode = process.returncode
@@ -223,7 +244,7 @@ def regionGroup(inRasterPath, aoiVectorPath, aoiRegionsPath):
     Polygonize raster into threholded regions, clip polygons to AOI.
     """
     # gdal_polygonize.py -nomask temp.tif -b 1 bar.gml
-    codeIn = ['gdal_polygonize.py','-nomask', '-f', 'ESRI Shapefile', inRasterPath, '-b', '1', 'rg.shp']
+    codeIn = ['gdal_polygonize.py', '-f', 'ESRI Shapefile', inRasterPath, '-b', '1', 'rg.shp']
     process = subprocess.Popen(codeIn,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out,err = process.communicate()
     errcode = process.returncode
@@ -244,32 +265,36 @@ def regionGroup(inRasterPath, aoiVectorPath, aoiRegionsPath):
         if errcode:
             raise RuntimeError('Error return code {0} in regionGroup ogr2ogr.\n{1}\n{2}'.format(errcode, out, err))
 
-
 # Utility function to collect statistics on a shapefile
 # @param
-#     [shape] - full path to the area to be summarized
-#     [statsResultTable] - full path to RCSV results
+#     [zonesShape] - full path to the area to be summarized
+#     [raster] -  full path to the raster 
+#     [reclassTable] - full path to the reclassification table
+#     [statsResultTable] - full path to CSV results
 # @return None
-# Example call: regionGroup('mosaic.tif', 'aoi.shp', 'aoiRegionGrouped.shp')
-def zonalStats(shape, statsResultTable):
+# Example call: regionGroup('aoiRegionGrouped.shp', 'mosiac.tif', 'reclass.csv', 'stats.csv')
+def zonalStats(zonesShape, raster, reclassTable, statsResultTable):
 
+    # max is count * maxVal where maxVal is the largest 'to' value in the reclassTable
+    [lowerLimitList, upperLimitList, toValueList] = reclassCSVColumns(reclassTable)
+    toValueList.sort()
+    max = toValueList.pop()
+    # rasterstats zonal_stats returns a dictionary for each Feature in the zonesShape vector in a list
+    zs = zonal_stats(zonesShape, raster, geojson_out=True, stats=['count', 'sum'])
     l = []
-    sqlString = "SELECT FID, OGR_GEOM_AREA, DN FROM {0} WHERE DN > 0 ORDER BY OGR_GEOM_AREA DESC".format(os.path.splitext(os.path.basename(shape))[0])
-    ds = ogr.Open(shape)
-    layer = ds.ExecuteSQL(sqlString)
-
-    for feature in layer:
-        l.append((feature.GetField("FID"), feature.GetField("OGR_GEOM_AREA"), feature.GetField("DN")))
+    # for each dictionary in the zs list returned by rasterstats
+    for d in zs: 
+        l.append((d['id'], int(d['properties']['count']) * 900, d['properties']['DN'], d['properties']['sum'], 
+                 d['properties']['count'],  int(d['properties']['count']) * max)) 
 
     f = open(statsResultTable,'wt')
     try:
         writer = csv.writer(f)
-        writer.writerow( ('Unique region ID', 'Area in square meters', 'Raster value') )
+        writer.writerow(('Unique region ID', 'Area in square meters', 'Raster value', 'Sum', 'Count', 'Count*maxVal'))
         for tup in l:
             writer.writerow(tup)
     finally:
         f.close()
-
 
 
 """
@@ -288,6 +313,7 @@ Overall steps:
 cloudMasksDict={}
 gapMasksDict={}
 reclassedTIFFs = []
+zeroOrOneTIFFs = []
 
 try:
 
@@ -310,30 +336,50 @@ try:
         gapMasks = [x.strip() for x in gapMasks]
         map(lambda x: addToDictionary(gapMasksDict, x), gapMasks)
 
-    #pdb.set_trace()
+    # assemble all quad products for the AOI
     for tiffFilename in swirTIFFs:
         srcRaster = gdal.Open(tiffFilename)
         band = srcRaster.GetRasterBand(1)
+	# get a numpy array of SWIR values
         srcBandArray = band.ReadAsArray()
+	# get wrs2 and quad ID used to tie SWIR to masks
         k=keyFromFilename(tiffFilename)
+	# apply masks first
         maskedArray = applyMasks(srcBandArray, cloudMasksDict.get(k), gapMasksDict.get(k))
+	# then reclass according to thresholds in the reclassTable
         reclassedArray = reclass(maskedArray, reclassTable)
-        writeArrayAsTIFF(reclassedArray, k+'temp.tif', srcRaster.RasterXSize, srcRaster.RasterYSize,
+	# write out the masked, reclassed SWIR as TIFF
+	# to be used as input to GDAL/OGR command line scripts
+        writeArrayAsTIFF(reclassedArray, k+'temp0.tif', srcRaster.RasterXSize, srcRaster.RasterYSize,
                           gdal.GDT_Byte, srcRaster.GetGeoTransform(), srcRaster.GetProjection())
+        cropToQuad(k+'temp0.tif', k+'temp.tif',k)
         reclassedTIFFs.append(k+'temp.tif')
-
+        # write out a 0 or 1 array which will be used to identify the boundary of all change
+        array01=np.where(maskedArray>0,1, 0)
+        writeArrayAsTIFF(array01, k+'array0.tif', srcRaster.RasterXSize, srcRaster.RasterYSize,
+                          gdal.GDT_Byte, srcRaster.GetGeoTransform(), srcRaster.GetProjection())
+        cropToQuad(k+'array0.tif', k+'array01.tif', k)
+        zeroOrOneTIFFs.append(k+'array01.tif')
+    # make the AOI a shape
     translateAOI(aoi, 'aoi.shp')
+    # combine the quad products into a single TIFF to be clipped
     mosaic(reclassedTIFFs, 'mosaic.tif')
-    regionGroup('mosaic.tif', 'aoi.shp', 'aoiRegionGrouped.shp')
-    stats=zonalStats('aoiRegionGrouped.shp', statsResultTable)
+    mosaic(zeroOrOneTIFFs, 'zeroOrOne.tif')
+    # aggregate into regions and clip
+    regionGroup('zeroOrOne.tif', 'aoi.shp', 'aoiRegionGrouped.shp')
+    # output region area and class
+    stats=zonalStats('aoiRegionGrouped.shp', 'mosaic.tif', reclassTable, statsResultTable)
     # clean up temporary files
     os.remove('mosaic.tif')
+    os.remove('zeroOrOne.tif')
     map(lambda x: os.remove(x), reclassedTIFFs)
+    map(lambda x: os.remove(x), zeroOrOneTIFFs)
     map(lambda x: os.remove(x), glob.glob('aoi.*'))
     map(lambda x: os.remove(x), glob.glob('aoiRegionGrouped.*'))
 except BaseException as(e):
     print e
     sys.exit()
 
+# dump stats on stdout. this isn't required but useful in testing standalone script 
 print open(statsResultTable, 'rt').read()
 
