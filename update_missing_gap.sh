da162ff7f59a2c1ec3d0028fbba532f20c16b75b
Daveism <daveism@gmail.com>
2017-11-07 13:25:29 +0000

    fix formissing gap files

---
+++ update_missing_gap.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+DIRECTORY=`dirname $0`
+echo $DIRECTORY
+
+#get the config file and make sure it will not do something delete all...
+configfile=$DIRECTORY/bash_config.cfg
+configfile_secured=$DIRECTORY/tmp_bash_config.cfg
+
+# check if the file contains something we don't want
+if egrep -q -v '^#|^[^ ]*=[^;]*' "$configfile"; then
+  # filter the original to a new file
+  egrep '^#|^[^ ]*=[^;&]*'  "$configfile" > "$configfile_secured"
+  configfile="$configfile_secured"
+fi
+
+#  now source it, either the original or the filtered variant
+source "$configfile"
+
+cd $path_projects/geoprocessing
+#attempt to redo dn
+./fix_missing_gap.py
